c50e7c9ebf8cabdfe0e904d0511b7ae165197e89
Abijith R <abijithrquora@gmail.com>
2023-08-04 23:44:32 +0530

    uploading build

--- src/Routes/Projects.js
+++ src/Routes/Projects.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar';
+import Bg from '../components/Bg';
+import Footer from '../components/Footer';
+import Skills from '../components/Skills';
 const Projects = () => {
   return (
 
     <div>
-      <Navbar></Navbar>
-      Projects
+      <Navbar/>
+      <Skills/>
+      <Bg head2="Full Stack Developer"  i
+      head1="MY PROJECTS" iurl="https://png.pngtree.com/thumb_back/fh260/background/20210408/pngtree-trendy-geometric-composition-of-silver-lines-on-black-neutral-background-for-image_605702.jpg"/>
+      
     </div>
   )
 }
