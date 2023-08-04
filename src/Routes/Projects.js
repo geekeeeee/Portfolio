64d4606149471391e517272c92083f2b813d43c6
Abijith R <abijithrquora@gmail.com>
2023-08-05 01:08:48 +0530

    published to firebase

--- src/Routes/Projects.js
+++ src/Routes/Projects.js
@@ -8,7 +8,6 @@ const Projects = () => {
 
     <div>
       <Navbar/>
-      <Skills/>
       <Bg head2="Full Stack Developer"  i
       head1="MY PROJECTS" iurl="https://png.pngtree.com/thumb_back/fh260/background/20210408/pngtree-trendy-geometric-composition-of-silver-lines-on-black-neutral-background-for-image_605702.jpg"/>
       
